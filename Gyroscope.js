2596baca68aa793f65fb2b8c24fb45675efa255c
Simon Werner <simonwerner@gmail.com>
2016-02-20 08:13:53 +1300

    Updated documentation

--- Gyroscope.js
+++ Gyroscope.js
@@ -23,22 +23,26 @@
 
 'use strict';
 
+// The datasheet documentation for the ITG 3200 can be found here:
+//
+
 var GYRO_ADDRESS = 0x68;
 
 var GYRO_RESET_ADDRESS = 0x3E;
-var GYRO_RESET_BYTE = 0x80;
+var GYRO_RESET_BYTE = 0x80;             // Power management register: refer to page 27 of datasheet
 
 var GYRO_SAMPLERATE_ADDRESS = 0x15;
-var GYRO_SAMPLERATE_DIVIDER = 0x00;
+var GYRO_SAMPLERATE_DIVIDER = 0x00;     // Sample rate divider: use default value 0x00 for full range: refer to pg 23
 
 var GYRO_DEGREES_ADDRESS = 0x16;
-var GYRO_DEGREES_VALUE = 0x18;      // Default
+var GYRO_DEGREES_VALUE = 0x18;          // gyro full scale range: refer to pg 24
 
 var GYRO_READ_ADDRESS = 0x1B;
 var GYRO_READ_LEN = 8;
 
 var GYRO_SENSITIVITY = 14.375;
 
+// 280 is the sensitivity. refer to pg 7 section temperature sensor.
 var TEMP_OFFSET = 0; // (35 - 13200 / 280);  ()
 var TEMP_SENSITIVITY = 280;
 
@@ -166,7 +170,6 @@ function convertBytes(byteLo, byteHi, offset, sensitivity) {
     // add the two bytes together (bit-shift x1)
     var byteVal;
 
-    // if we have a negative byte value
     if (byteHi > 15) {
         byteHi = byteHi & 0x0F;                         // don't count the first 4 bits
         byteVal = 0x0FFF - (byteLo + (byteHi << 8));    // Make the value negative
